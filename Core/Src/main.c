326da673aa4fc3a3b9977c8cf4879b8a017985ca
Johannes Schnitzler <johannes.schnitzler@ladon-energy.com>
2025-10-18 22:38:28 +0200

    Just a test, if the version control works

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,6 +26,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+/*This is a test if the version control works : TEST 01*/
 
 /* USER CODE END Includes */
 
